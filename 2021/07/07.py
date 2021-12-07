6b355194bb86f52a7c396202087df6085a638d1e
Christos Kasketis <kasketis@gmail.com>
2021-12-07 22:27:07 +0200

    Solve day 07 2021

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -4,5 +4,5 @@ with open("input.txt") as f:
     crabs = list(map(int, f.read().split(",")))
 
 m = median(crabs)
-min_fuel = sum(int(abs(crab - m)) for crab in crabs)
+min_fuel = sum(abs(crab - m) for crab in crabs)
 print(min_fuel)
--- 2021/07/07b.py
+++ 2021/07/07b.py
@@ -1,7 +1,7 @@
 import sys
 
 with open("input.txt") as f:
-    crabs = sorted(list(map(int, f.read().split(","))))
+    crabs = sorted(map(int, f.read().split(",")))
 
 min_fuel = sys.maxsize
 for x in range(crabs[0], crabs[-1] + 1):
